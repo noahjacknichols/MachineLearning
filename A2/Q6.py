cf5950dca2d2641f17cbed13e056563e573d20f7
noah nichols <noahjacknichols@gmail.com>
2019-11-05 16:30:00 -0500

    made nick happy

--- A2/Q6.py
+++ A2/Q6.py
@@ -10,8 +10,10 @@ data = pandas.read_csv('data.csv', names=names)
 print(data)
 
 tree = KDTree(data)
-# k = 3
-dist, ind = tree.query([[8., 1000., 2200., -1.]], k=3)
 
-# print(dist)
-print(ind)
+#we then query the KDTree for the k-nn of the new node with values size=1000, rent=2200
+#for k=1
+dist, ind = tree.query([[8., 1000., 2200., -1.]], k=1)
+#we add 1 to index to get the ID of the closest property
+
+print(ind+1) 
